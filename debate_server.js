fb9736612a6bbb537380069280aa035eedd4b6a9
SummerLi <xl1029@nyu.edu>
2018-12-17 17:17:24 -0500

    Feat
    Updated the server side node.js for the debate.proto

---
+++ debate_server.js
@@ -0,0 +1,53 @@
+function Answer(call, callback){
+	// all strings should be case insensitive
+	const question = call.request.question.toLowerCase();
+	const answers[] = ["goes too far","doesn't go too far enough"];
+	// if the question doesn't start with "why", "what", "how", "who"
+	if(question.slice(0, 3)!== 'why' || question.slice(0, 4) !== 'what' || question.slice(0, 3) !== 'how' || question.slice(0, 3) !== 'who'){
+		callback(null, {answer: 'your 3 cent titanium tax ' + answers[Math.round(Math.random())]});
+	}
+	else{
+		const question_sub = question.replace("you", "I").replace("your", "my");
+		// make an RPC call with 'question' to the CampaignManager.Retort service
+		const retort = ?;
+		callback(null, {answer: 'You asked me ' + question_sub + 'but I want to say that' + retort});
+	}
+}
+
+
+function Elaborate(call, callback){
+	const numbers = [...call.request.blah_run];
+	const topic = call.request.topic;
+	// when blah_run is empty
+	if(numbers === NULL){
+		callback(null, {answer: topic});
+	}
+	// when blah_run has one run
+	else if(numbers.length === 1){
+		let string = '';
+		for(let i = 0; i < numbers[0]; i++){
+			string += 'blah ';
+		}
+		callback(null, {answer: string + topic});
+	}
+	else{
+		let string = '';
+		numbers.forEach(number => {
+			for(let i = 0; i < number; i++){
+				string += 'blah ';
+			}
+			string += topic;
+		})
+		callback(null, {answer: string});
+	}
+}
+
+
+
+function main() {
+  var server = new grpc.Server();
+  server.addProtoService(hello_proto.Greeter.service,
+                         {sayHello: sayHello, sayHelloAgain: sayHelloAgain});
+  server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
+  server.start();
+}
